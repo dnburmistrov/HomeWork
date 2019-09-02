8d1a33be41ca96d3a2c43dab4d6ecd173573e9f7
Denys Burmistrov <dnburmistrov@gmail.com>
2019-09-02 21:14:17 +0300

    added 1.2

---
+++ lesson6/1.2.php
@@ -0,0 +1,25 @@
+<?php
+function add($a, $b)
+{
+    return $a + $b;
+}
+
+function sub($a, $b)
+{
+    return $a - $b;
+}
+
+function operation($numOne, $numTwo, $str)
+{
+    switch ($str) {
+        case 'add':
+            return add($numOne, $numTwo);
+        case 'sub':
+            return sub($numOne, $numTwo);
+
+    }
+}
+
+$result = operation(509, 675, 'sub');
+printf("\$result = %s", $result);
+?>
