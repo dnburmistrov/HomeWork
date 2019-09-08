cf5de12b63c971adb74bce675575ed6a8a49f8db
Denys Burmistrov <dnburmistrov@gmail.com>
2019-09-08 12:02:45 +0300

    Update some code

--- lesson6/2.1.php
+++ lesson6/2.1.php
@@ -2,10 +2,11 @@
 function brainstorm(int $a)
 {
     if ($a != 0) {
-        echo $a % 10 . '<br>';
+        echo $a % 10;
         brainstorm($a / 10);
     } else return;
 }
 
 $a = rand(1, 56751);
+printf("Данно натуральное число - %s <br>", $a);
 $a = brainstorm($a);
--- lesson6/2.2.php
+++ lesson6/2.2.php
@@ -3,8 +3,12 @@ $str = '1,2,2,3,3,3,4,4,4,4,5,5,5,5,5,6,6,6,6,6,6,7,7,7,7,7,7,7,8,8,8,8,8,8,8,8'
 $arrK = explode(",", $str);
 $n = rand(1, 8);
 echo $n . '<br>';
-foreach ($arrK as $key => $value) {
-    if ($key >= $n) {
-        break;
-    } else printf($value);
+function myFunction(array $arr, int $int)
+{
+    foreach ($arr as $key => $value) {
+        if ($key >= $int) {
+            break;
+        } else printf($value);
+    }
 }
+myFunction($arrK,$n);
